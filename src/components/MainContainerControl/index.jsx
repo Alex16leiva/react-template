83ff64d3bb51433ec84d2e67bb83aa4bd10e8050
Alexander Castellanos <rcastellanos@diunsa.hn>
2026-06-26 12:47:21 -0600

    Refactory de contenedor Principal

--- src/components/MainContainerControl/index.jsx
+++ src/components/MainContainerControl/index.jsx
@@ -3,12 +3,8 @@ import { Box } from '@mui/material';
 export const MainContainerControl = ({ children, sx }) => (
   <Box
     sx={{
-      flex: 1,
-      minWidth: 0,
-      minHeight: 0,
+      height:'calc(100% - 70px)',
       overflow: 'hidden',
-      width: '100%',
-      height: '100%',
       ...sx,
     }}
   >
