a31422834e2df99d85b18f3073b7551470167dcc
Alexander Castellanos <rcastellanos@diunsa.hn>
2026-06-26 08:48:31 -0600

    Se creo el control generico MainContainerControl

--- src/components/MainContainerControl/index.jsx
+++ src/components/MainContainerControl/index.jsx
@@ -1,21 +1,17 @@
 import { Box } from '@mui/material';
-import { useSelector } from 'react-redux';
-import { selectSidebarOpen, selectSidebarWidth, SIDEBAR_COLLAPSED_WIDTH } from '../../store/sidebarSlice';
 
-export const MainContainerControl = ({ children }) => {
-  const open = useSelector(selectSidebarOpen);
-  const width = useSelector(selectSidebarWidth);
-
-  return (
-    <Box
-      sx={{
-        flex: 1,
-        minWidth: 0,
-        transition: 'margin-left 0.2s ease',
-        ml: `${open ? width : SIDEBAR_COLLAPSED_WIDTH}px`,
-      }}
-    >
-      {children}
-    </Box>
-  );
-};
+export const MainContainerControl = ({ children, sx }) => (
+  <Box
+    sx={{
+      flex: 1,
+      minWidth: 0,
+      minHeight: 0,
+      overflow: 'hidden',
+      width: '100%',
+      height: '100%',
+      ...sx,
+    }}
+  >
+    {children}
+  </Box>
+);
